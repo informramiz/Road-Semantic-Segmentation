2f675f7482f3cf8daa6773382dfe280fd986269b
Ramiz Raja <informramiz@gmail.com>
2017-10-11 06:48:50 +0500

    refactor: Remove images count limit, set it to total

--- helper.py
+++ helper.py
@@ -79,7 +79,7 @@ def gen_batch_function(data_folder, image_shape):
         background_color = np.array([255, 0, 0])
 
         random.shuffle(image_paths)
-        for batch_i in range(0, 50, batch_size):
+        for batch_i in range(0, len(image_paths), batch_size):
             images = []
             gt_images = []
             for image_file in image_paths[batch_i:batch_i+batch_size]:
